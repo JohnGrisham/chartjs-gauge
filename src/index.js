e146181ebc244f9c6ca3c85643feefc123b493ca
John Grisham <john.grisham@gatewayapps.com>
2021-09-24 13:26:35 -0500

    Removed some files udpate return to be gauge controller.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-import Chart from 'chart.js';
-
 import GaugeController from './controllers/controller.gauge';
 
-Chart.controllers.gauge = GaugeController;
-Chart.Gauge = (context, config) => {
-  config.type = 'gauge';
-  config.id = 'gauge';
-
-  return new Chart(context, config);
-};
-
-export default Chart.Gauge;
+export default GaugeController;
